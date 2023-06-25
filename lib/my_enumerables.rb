5aa3b0b3cc70ea73444cb6027da512f5a0b72e8e
Cake <cakemancerr@gmail.com>
2023-06-26 00:59:17 +0200

    feat: add solutions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,64 @@
+# frozen_string_literal: true
+
 module Enumerable
-  # Your code goes here
+  def my_all?
+    my_each { |item| return false unless yield(item) }
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield(item) }
+    false
+  end
+
+  def my_count
+    if block_given?
+      count = 0
+      my_each { |item| count += 1 if yield(item) }
+      return count
+    end
+    size
+  end
+
+  def my_each_with_index
+    if block_given?
+      index = 0
+      my_each do |item|
+        yield(item, index)
+        index += 1
+      end
+      return self
+    end
+    size
+  end
+
+  def my_inject(initial)
+    return unless block_given?
+
+    my_each { |item| initial = yield(initial, item) }
+    initial
+  end
+
+  def my_map
+    return unless block_given?
+
+    result = []
+    my_each { |item| result << yield(item) }
+    result
+  end
+
+  def my_none?
+    my_each { |item| return false if yield(item) }
+    true
+  end
+
+  def my_select
+    return unless block_given?
+
+    result = []
+    my_each { |item| result << item if yield(item) }
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +67,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  # noinspection RubyForLoopInspection
+  def my_each
+    for item in self
+      yield item
+    end
+  end
 end
